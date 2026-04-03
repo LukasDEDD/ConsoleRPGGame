b59aaf735f284063c375f7b3b2ce2ae99f4adb3e
LukasDD82 <lukas.simek.de@gmail.com>
2026-04-03 20:50:22 +0200

    Item + ItemRepository - done

--- ConsoleRPGGame/src/main/java/com/ConsoleRPGGame/domain/item/Item.java
+++ ConsoleRPGGame/src/main/java/com/ConsoleRPGGame/domain/item/Item.java
@@ -1,5 +1,59 @@
 package com.ConsoleRPGGame.domain.item;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "item")
 public class Item {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+  private String type;
+  private int power;
+
+  public Item() {
+  }
+
+  public Item(Long id, String name, String type, int power) {
+    this.id = id;
+    this.name = name;
+    this.type = type;
+    this.power = power;
+  }
+
+  public int getPower() {
+    return power;
+  }
+
+  public void setPower(int power) {
+    this.power = power;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
 }
