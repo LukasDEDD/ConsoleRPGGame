ca00a472ede02c42c93294e04797b8c824c6d1e9
LukasDD82 <lukas.simek.de@gmail.com>
2026-04-04 22:58:45 +0200

    MeleeAttack - done

--- ConsoleRPGGame/src/main/java/com/ConsoleRPGGame/domain/combat/MeleeAttack.java
+++ ConsoleRPGGame/src/main/java/com/ConsoleRPGGame/domain/combat/MeleeAttack.java
@@ -1,6 +1,19 @@
 package com.ConsoleRPGGame.domain.combat;
 
-public interface MeleeAttack {
 
+import com.ConsoleRPGGame.domain.creature.Creature;
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class MeleeAttack implements AttackStrategy {
+
+  @Override
+  public int calculateDamage(Creature attacker, Creature defender) {
+
+    int damage = attacker.getStrength() - defender.getDefense();
+
+    return Math.max(0, damage);
+  }
 
 }
