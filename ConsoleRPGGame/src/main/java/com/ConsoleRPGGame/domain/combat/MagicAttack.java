a795264e872fdd89e43885b7f8104fe48d4eb9f4
LukasDD82 <lukas.simek.de@gmail.com>
2026-04-04 23:08:38 +0200

    MagicAttack - done

--- ConsoleRPGGame/src/main/java/com/ConsoleRPGGame/domain/combat/MagicAttack.java
+++ ConsoleRPGGame/src/main/java/com/ConsoleRPGGame/domain/combat/MagicAttack.java
@@ -1,4 +1,18 @@
 package com.ConsoleRPGGame.domain.combat;
 
-public class MagicAttack {
+
+import com.ConsoleRPGGame.domain.creature.Creature;
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class MagicAttack implements AttackStrategy{
+
+  @Override
+  public int calculateDamage(Creature attacker, Creature defender) {
+
+    int damage = attacker.getStrength() - (defender.getDefense()/2);
+
+    return Math.max(0, damage);
+  }
 }
