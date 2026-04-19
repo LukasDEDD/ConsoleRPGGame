de1fcd1056206fa664738b5789462da7aec8d719
LukasDD82 <lukas.simek.de@gmail.com>
2026-04-19 19:52:22 +0200

    InputReader done

--- ConsoleRPGGame/src/main/java/com/ConsoleRPGGame/infrastructure/input/InputReader.java
+++ ConsoleRPGGame/src/main/java/com/ConsoleRPGGame/infrastructure/input/InputReader.java
@@ -1,4 +1,27 @@
 package com.ConsoleRPGGame.infrastructure.input;
 
+import org.springframework.stereotype.Component;
+
+import java.util.Scanner;
+
+@Component
 public class InputReader {
+  private final Scanner scanner;
+
+  public InputReader(Scanner scanner) {
+    this.scanner = scanner;
+  }
+
+  public String getCommand() {
+    System.out.print("\n> ");
+    return scanner.nextLine().trim().toLowerCase();
+  }
+
+  public int getIntInput() {
+    while (!scanner.hasNextInt()) {
+      System.out.println("That's not a number! Try again:");
+      scanner.next();
+    }
+    return scanner.nextInt();
+  }
 }
